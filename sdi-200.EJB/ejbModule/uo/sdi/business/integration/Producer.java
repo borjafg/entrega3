c3e9aa5f99d03a1f0b4b673f03e349af9baa5002
borjafg <borjafg_95@hotmail.com>
2016-06-10 19:06:10 +0200

    Cambios minimos en presentacion

--- sdi-200.EJB/ejbModule/uo/sdi/business/integration/Producer.java
+++ sdi-200.EJB/ejbModule/uo/sdi/business/integration/Producer.java
@@ -8,8 +8,6 @@ import javax.jms.MapMessage;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 
-import uo.sdi.client.util.Jndi;
-
 public class Producer {
     private static final String JMS_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
     private static final String CORREO_TOPIC = "jms/topic/MensajeriaCorreo";
@@ -35,8 +33,8 @@ public class Producer {
 	// Crear conexion e iniciar una sesion
 	// ===================================
 
-	ConnectionFactory factory = Jndi
-		.getConnectionFactory(JMS_CONNECTION_FACTORY);
+	ConnectionFactory factory = null;/*= Jndi
+		.getConnectionFactory(JMS_CONNECTION_FACTORY)*/ 
 
 	con = factory.createConnection(USER, PASSWORD);
 	session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -45,7 +43,7 @@ public class Producer {
 	// Crear productor de mensajes
 	// ===========================
 
-	Destination topic = Jndi.getDestination(CORREO_TOPIC);
+	Destination topic = null;/*= Jndi.getDestination(CORREO_TOPIC)*/
 	sender = session.createProducer(topic);
 
 	// =======
