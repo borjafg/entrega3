5f1de4118281c9890de3819a5f3d456621400182
borjafg <borjafg_95@hotmail.com>
2016-06-19 18:20:19 +0200

    Arreglos mínimos.

--- sdi-200.EJB/ejbModule/uo/sdi/infrastructure/Factories.java
+++ sdi-200.EJB/ejbModule/uo/sdi/infrastructure/Factories.java
@@ -10,8 +10,10 @@ public class Factories {
     // public static PersistenceFactory persistence = new PersistenceFactory();
 
     private static String CONFIG_FILE = "/factories.properties";
+    
     public static ServicesFactory services = (ServicesFactory) FactoriesHelper
 	    .createFactory(CONFIG_FILE, "SERVICES_FACTORY");
+    
     public static PersistenceFactory persistence = (PersistenceFactory) FactoriesHelper
 	    .createFactory(CONFIG_FILE, "PERSISTENCE_FACTORY");
 }
